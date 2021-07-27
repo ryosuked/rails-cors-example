8d4ee37dcb9d1f78b00ad21480ea332582615361
sk <sk@local>
2021-07-27 21:39:37 +0900

    Add rack-cors configurations

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  match '*path' => 'options_request#preflight', via: :options
+
+  get 'echo', action: :echo, controller: 'echo'
+#  match 'echo', via: :options, action: :echo, controller: 'echo'
 end
